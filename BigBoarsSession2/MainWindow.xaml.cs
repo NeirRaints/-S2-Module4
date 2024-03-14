d7159ffe8ae03aff347dc27ce1fa8dd7204c8484
Владилен Турышев <niggative99@mail.ru>
2024-03-14 16:30:46 +0500

    Добавьте файлы проекта.

---
+++ BigBoarsSession2/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Newtonsoft.Json;
+
+namespace BigBoarsSession2
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<PersonLocations> personLocationsList = new ObservableCollection<PersonLocations>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            LoadPersonLocations();
+        }
+
+        private async void LoadPersonLocations()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "https://localhost:7177/api/persons";
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    IEnumerable<PersonLocations> personLocations = JsonConvert.DeserializeObject<IEnumerable<PersonLocations>>(json);
+
+                    foreach (var personLocation in personLocations)
+                    {
+                        personLocationsList.Add(personLocation);
+                    }
+
+                    // Привязать personLocationsList к элементу управления WPF
+                    // Например:
+                    TestGrid.ItemsSource = personLocationsList;
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка при загрузке данных");
+                }
+            }
+        }
+    }
+}
---
+++ BigBoarsSession2/PersonLocations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBoarsSession2
+{
+    public class PersonLocations
+    {
+        public string PersonCode { get; set; }
+        public string PersonRole { get; set; }
+        public int LastSecurityPointNumber { get; set; }
+        public string LastSecurityPointDirection { get; set; }
+        public DateTime LastSecurityPointTime { get; set; }
+    }
+}

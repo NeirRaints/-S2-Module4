2a70bd344b9d8680503ecff21d9cb488d7e7666e
Владилен Турышев <niggative99@mail.ru>
2024-03-17 19:21:18 +0500

    Убрал лишние ресурсы

--- BigBoarsSession2/Properties/Resources.Designer.cs
+++ BigBoarsSession2/Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace BigBoarsSession2.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Приемный_покой {
-            get {
-                object obj = ResourceManager.GetObject("Приемный_покой", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
